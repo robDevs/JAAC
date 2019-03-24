496753ec8b3acdffcc9ba4e5bcf0c90c600dac29
robDevs <robbdevv@gmail.com>
2019-03-24 17:25:38 -0600

    fix control defines

--- src/common.h
+++ src/common.h
@@ -17,18 +17,16 @@
 #include <string.h>
 
 // For Snes style buffalo classic gamepad tested on Gallium OS
-#define GAMEPAD_BUFFALOSNES_BUTTON_B        1
-#define GAMEPAD_BUFFALOSNES_BUTTON_A        0
-#define GAMEPAD_BUFFALOSNES_BUTTON_Y        3
-#define GAMEPAD_BUFFALOSNES_BUTTON_X        2
-#define GAMEPAD_BUFFALOSNES_BUTTON_UP       1 //marks the axis
-#define GAMEPAD_BUFFALOSNES_BUTTON_DOWN     0 // marks the axis
-#define GAMEPAD_BUFFALOSNES_BUTTON_LEFT     14
-#define GAMEPAD_BUFFALOSNES_BUTTON_RIGHT    15
-#define GAMEPAD_BUFFALOSNES_BUTTON_START    7
-#define GAMEPAD_BUFFALOSNES_BUTTON_SELECT   6
-#define GAMEPAD_BUFFALOSNES_BUTTON_L        4
-#define GAMEPAD_BUFFALOSNES_BUTTON_R        5
+#define GAMEPAD_BUFFALOSNES_BUTTON_B            1
+#define GAMEPAD_BUFFALOSNES_BUTTON_A            0
+#define GAMEPAD_BUFFALOSNES_BUTTON_Y            3
+#define GAMEPAD_BUFFALOSNES_BUTTON_X            2
+#define GAMEPAD_BUFFALOSNES_BUTTON_UP_DOWN      1 //marks the axis
+#define GAMEPAD_BUFFALOSNES_BUTTON_LEFT_RIGHT   0 // marks the axis
+#define GAMEPAD_BUFFALOSNES_BUTTON_START        7
+#define GAMEPAD_BUFFALOSNES_BUTTON_SELECT       6
+#define GAMEPAD_BUFFALOSNES_BUTTON_L            4
+#define GAMEPAD_BUFFALOSNES_BUTTON_R            5
 
 #define menu  0
 #define game  1
